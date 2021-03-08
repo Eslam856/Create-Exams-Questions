f9402518610b23effdfc683cb19d736d297645d0
Eslam Muhamed Taha Ibrahim <45505496+Eslam856@users.noreply.github.com>
2021-03-08 22:00:19 +0200

    first commit

---
+++ Create_Exam/Controllers/ExamController.cs
@@ -0,0 +1,56 @@
+﻿using Create_Exam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Create_Exam.Controllers
+{
+    public class ExamController : Controller
+    {
+        OnlineExaminationDBEntities db = new OnlineExaminationDBEntities();
+        // GET: Exam
+        
+        public ActionResult CreateExam()
+        {
+            List<Question> allQuestions = new List<Question>()
+            {
+                new Question{ID=1, Text="Question 1", IsSingleAnswer=true},
+                new Question{ID=2, Text="Question 2", IsSingleAnswer=false},
+                new Question{ID=3, Text="Question 3", IsSingleAnswer=true},
+                new Question{ID=4, Text="Question 4", IsSingleAnswer=true},
+                new Question{ID=5, Text="Question 5", IsSingleAnswer=false},
+                new Question{ID=6, Text="Question 6", IsSingleAnswer=true},
+                new Question{ID=7, Text="Question 7", IsSingleAnswer=true},
+                new Question{ID=8, Text="Question 8", IsSingleAnswer=false},
+                new Question{ID=9, Text="Question 9", IsSingleAnswer=true}
+            };
+            return View(allQuestions);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var Questions = db.Questions.ToList();
+            ViewBag.questions = Questions;
+            return View(ViewBag.questions);
+        }
+        [HttpPost]
+        public ActionResult Create(Exam exam)
+        {
+            string[] EAns = exam.examAnswers.Split(',');
+            foreach (var item in EAns)
+            {
+                exam.ExamAnswers.Add(new ExamAnswer { ExamID = exam.ID,AnswerID =Convert.ToInt32(item)});
+            }
+            db.Exams.Add(exam);
+            db.SaveChanges();
+            return View();
+        }
+
+       
+
+
+    }
+
+}
---
+++ Create_Exam/Controllers/QuestionController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Create_Exam.Models;
+
+namespace Create_Exam.Controllers
+{
+    public class QuestionController : Controller
+    {
+        OnlineExaminationDBEntities db = new OnlineExaminationDBEntities();
+        [HttpGet]
+        // GET: Question
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateQuestion()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateQuestion(Question q)
+        {
+            string[] ans = q.AnswerText.Split('|');
+            foreach (var item in ans)
+            {
+                q.Answers.Add(new Answer { Text = item.Replace("#", ""),IsTrueAnswer=item.Trim().StartsWith("#")});
+            }
+            db.Questions.Add(q);
+            db.SaveChanges();
+            return RedirectToAction("Index","Exam");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var question = db.Questions.SingleOrDefault(q => q.ID == id);
+
+            return View(question);
+        }
+
+        public ActionResult AllQuestions()
+        {
+            List<Question> Questions = db.Questions.ToList();
+            return View(Questions);
+        }
+
+        [HttpGet]
+
+        public ActionResult Delete(int id)
+        {
+            var question = db.Questions.SingleOrDefault(q => q.ID == id);
+            return View(question);
+        }
+
+        // POST: AuthorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Question question)
+        {
+
+            try
+            {
+
+                db.Questions.Remove(question);
+
+                return RedirectToAction("Question", "AllQuestions");
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+
+        }
+
+
+
+    }
+
+}
